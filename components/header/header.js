a653b54fec44df41417e25572c02293061dbe29a
chichiwattyp2 <53357288+chichiwattyp2@users.noreply.github.com>
2020-12-24 01:14:19 +0200

    Add files via upload

--- components/header/header.js
+++ components/header/header.js
@@ -1,10 +1,9 @@
 class PageHeader extends HTMLElement {
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    const template = `
+        const template = `
             <style>
-
                 a {
                     display: grid;
                     grid-template-columns: auto auto;
@@ -14,7 +13,6 @@ class PageHeader extends HTMLElement {
                     height: 5em;
                     width: 12.5em;
                     padding-left: 1.875em;
-
                 }
 
                 img {
@@ -24,102 +22,34 @@ class PageHeader extends HTMLElement {
                 }
 
                 p {
-                    font-family: ClobberGroteskW01-Medium;
+                    font-family: Chakra Petch;
                     font-style: normal;
                     font-weight: bold;
                     line-height: 1.625em;
                     padding-left: 0.625em;
-                    margin-left: 1em;
+                    margin: 0.3125em;
                     font-size: 1.25em;
                     color: black;
                 }
 
-                 .lead {
-      font-family: ClobberGroteskW01-Medium;
-      font-style: normal;
-      font-weight: bold;
-      font-size: 1.75em;
-      line-height: 140%;
-    }
+                .minor-text {
+                    font-size: 1em;
+                }
+            </style>
 
-    .buttons > button,
-    .buttons > a {
-        margin-left: 3.125em;
-        cursor: pointer;
-    }
-
-    
-    .primary-button,
-    .passive-button {
-      display: inline-block;
-      font-family: ClobberGroteskW01-Medium;
-      font-style: normal;
-      font-weight: 600;
-      font-size: 1em;
-      line-height: 1.25em;
-      height: 3.5em;
-      border: none;
-      border: 1.5px solid black;
-      box-sizing: border-box;
-      box-shadow: 10px 10px 0px black;
-      border-radius: 10px;
-      padding: 1em 1.25em;
-      margin: 1.875em 0;
-      text-decoration: none;
-      color: black;
-    }
-
-    .primary-button:active,
-    .passive-button:active {
-      box-shadow: 5px 5px 0px #000000;
-      transform: translateY(4px);
-    }
-
-    .primary-button {
-      background: var(--primary-color);
-    }
-
-    .primary-button:hover {
-      background: var(--primary-color-dark);
-    }
-
-    .passive-button {
-      background: var(--passive-color);
-    }
-
-    .passive-button:hover {
-      background: var(--passive-color-dark);
-    }
-
-    .buttons > .publish-disabled {
-        cursor: default;
-        opacity: 0.4;
-    }
-  </style>
-            
-
-           
-<div class="buttons">
-      <a href="/public/social.html" id="social" class="primary-button">Social
-        </a>
-   
- <a href="/public/index.html" id="explore" class="primary-button">Explore
-        </a>
-      
-
-</div>
+            <a href="/studio">
+                <img class="logo-img" alt="logo" />
+            </a>
         `;
 
-    let shadow = this.attachShadow({ mode: "open" });
-    shadow.innerHTML = template;
-  }
+        let shadow = this.attachShadow({ mode: 'open' });
+        shadow.innerHTML = template
+    }
 
-  connectedCallback() {
-    this.classList.add("page-header");
-    this.shadowRoot.querySelector(".logo-img").src = `${this.getAttribute(
-      "assetsUrl"
-    )}/img/logo.png`;
-  }
+    connectedCallback() {
+        this.classList.add('page-header');
+        this.shadowRoot.querySelector('.logo-img').src = `${this.getAttribute('assetsUrl')}/img/logo.png`;
+    }
 }
 
-customElements.define("page-header", PageHeader);
+customElements.define('page-header', PageHeader);
